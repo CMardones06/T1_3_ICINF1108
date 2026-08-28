19197c90f82acf6d3f2e5468d066a209eaf8132a
keisy-dayana <epul.keisy@gmail.com>
2026-08-28 15:27:45 -0400

    feat: Estandarización de respuestas de students y se agrega manejo de errores

--- app/main.py
+++ app/main.py
@@ -1,8 +1,13 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, status
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse
+from fastapi.exceptions import RequestValidationError
+from starlette.exceptions import HTTPException as StarletteHTTPException
 
-from icinf1108.app.pets.pets_controller import router as pets_router
-from icinf1108.app.students.students_controller import router as students_router
+from app.shared.schemas import ApiResponse
+
+from app.pets.pets_controller import router as pets_router
+from app.students.students_controller import router as students_router
 
 
 def create_app() -> FastAPI:
@@ -24,7 +29,76 @@ def create_app() -> FastAPI:
     app.include_router(students_router)
     app.include_router(pets_router)
 
+    @app.exception_handler(StarletteHTTPException)
+    async def http_exception_handler(request: Request, exc: StarletteHTTPException):
+        mensajes_traduc = {
+            400: "Solicitud incorrecta",
+            401: "No autorizado",
+            403: "Acceso denegado",
+            404: "Recurso no encontrado",
+            405: "Método no permitido",
+            409: "Conflicto (el recurso ya existe)"
+        }
+
+        mensaje_traducido = mensajes_traduc.get(exc.status_code, str(exc.detail))
+        return JSONResponse(
+            status_code=exc.status_code,
+            content=ApiResponse(
+                success=False,
+                status_code=exc.status_code,
+                message=exc.detail,
+                data=None
+            ).model_dump()
+        )
+
+    @app.exception_handler(RequestValidationError)
+    async def validation_exception_handler(request: Request, exc: RequestValidationError):
+        return JSONResponse(
+            status_code=422,
+            content=ApiResponse(
+                success=False,
+                status_code=422,
+                message="Error de validación en los datos enviados",
+                data=exc.errors()
+            ).model_dump()
+        )
+
+    @app.exception_handler(StarletteHTTPException)
+    async def http_exception_handler(request: Request, exc: StarletteHTTPException):
+        api_response = ApiResponse(
+            success=False,
+            status_code=exc.status_code,
+            message=str(exc.detail),
+            data=None
+        )
+        return JSONResponse(
+            status_code=exc.status_code,
+            content=api_response.model_dump()
+        )
+
+    @app.exception_handler(RequestValidationError)
+    async def validation_exception_handler(request: Request, exc: RequestValidationError):
+        api_response = ApiResponse(
+            success=False,
+            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+            message="Error de validación en los datos enviados",
+            data=exc.errors()
+        )
+        return JSONResponse(
+            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+            content=api_response.model_dump()
+        )
+
+    @app.exception_handler(Exception)
+    async def global_exception_handler(request: Request, exc: Exception):
+            return JSONResponse(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                content=ApiResponse(
+                    success=False,
+                    status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                    message="Error interno del servidor",
+                    data=str(exc)
+                ).model_dump()
+            )
     return app
-
-
 app = create_app()
--- app/students/students_controller.py
+++ app/students/students_controller.py
@@ -1,35 +1,64 @@
 from fastapi import APIRouter
+from fastapi import status
 
-from icinf1108.app.pets.pets_service import pets_service
-from icinf1108.app.students.students_schemas import CreateStudentDto, Student, UpdateStudentDto
-from icinf1108.app.students.students_service import students_service
+from app.pets.pets_service import pets_service
+from app.students.students_schemas import CreateStudentDto, Student, UpdateStudentDto
+from app.students.students_service import students_service
+#Clase estandarizada
+from app.shared.schemas import ApiResponse
 
 router = APIRouter(prefix="/api/students", tags=["Students"])
 
 
 @router.get("")
-def find_all() -> list[Student]:
-    return students_service.find_all()
-
+def find_all() -> ApiResponse[list[Student]]:
+    data = students_service.find_all()
+    return ApiResponse(
+        success=True,
+        status_code=status.HTTP_200_OK,
+        message="Lista de estudiantes obtenida exitosamente!",
+        data=data
+    )
 
 @router.get("/{student_id}")
-def find_by_id(student_id: str) -> Student:
-    return students_service.find_by_id(student_id)
+def find_by_id(student_id: str) -> ApiResponse[Student]:
+    data=students_service.find_by_id(student_id)
+    return ApiResponse(
+        success=True,
+        status_code=status.HTTP_200_OK,
+        message="Estudiante encontrado!",
+        data=data
+    )
 
-
-@router.post("", status_code=201)
-def create(body: CreateStudentDto) -> Student:
-    return students_service.create(body)
-
+@router.post("", status_code=status.HTTP_201_CREATED)
+def create(body: CreateStudentDto) -> ApiResponse[Student]:
+    data=students_service.create(body)
+    return ApiResponse(
+        success=True,
+        status_code=status.HTTP_201_CREATED,
+        message="Estudiante creado exitosamente!!",
+        data=data
+    )
 
 @router.patch("/{student_id}")
-def update(student_id: str, body: UpdateStudentDto) -> Student:
-    return students_service.update(student_id, body)
+def update(student_id: str, body: UpdateStudentDto) -> ApiResponse[Student]:
+    data=students_service.update(student_id, body)
+    return ApiResponse(
+        success=True,
+        status_code=status.HTTP_200_OK,
+        message="Estudiante fue actualizado correctamente",
+        data=data
+    )
 
 
 @router.delete("/{student_id}")
-def delete(student_id: str) -> Student:
+def delete(student_id: str) -> ApiResponse[Student]:
     deleted = students_service.delete(student_id)
     pets_service.delete_all_for_student(student_id)
 
-    return deleted
+    return ApiResponse(
+        success=True,
+        status_code=status.HTTP_200_OK,
+        message="Estudiante y mascotas vinculadas fueron eliminadas correctamente",
+        data=deleted
+    )
